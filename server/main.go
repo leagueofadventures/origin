93fb3ef03a672b46a7c810297a835582200b6e3f
gamb1t <i88m@bk.ru>
2026-06-02 18:15:40 +0300

    Update main.go

--- server/main.go
+++ server/main.go
@@ -1140,7 +1140,7 @@ func main() {
 
 	databaseURL := os.Getenv("DATABASE_URL")
 	if databaseURL == "" {
-		log.Fatal("DATABASE_URL не установлена")
+		databaseURL = "127.0.0.1
 	}
 	var err error
 	db, err = sql.Open("postgres", databaseURL)
